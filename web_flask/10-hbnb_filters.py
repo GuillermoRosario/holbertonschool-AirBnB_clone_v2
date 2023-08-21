9d53f9e94831022f633a2533ff7cbd18fed7c732
GuillermoRosario <guillermorosario628@gmail.com>
2023-08-20 22:41:41 -0400

    Html Correction

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
+"""Starts a Flask web app
+Listens to port 5000 on 0.0.0.0
 """
-Module with Script that starts the 
-HBNB clone flask web application
-"""
-from flask import Flask, render_template
 from models import storage
-from models.state import State
-from models.amenity import Amenity
-"""
-Script that starts a Flask
-Web app for my HBNB clone project
-"""
-app = Flask(__name__)
-    
+from flask import Flask, render_template
 
-@app.route("/hbnb_filters", strict_slashes=False)
-def displayHBNB():
-    """
-    Renders the HTML of HBNB clone project,
-    with database information included
-    """
-    states = storage.all(State).values()
-    amenities = storage.all(Amenity).values()
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """Displays HBNB filters on rendered page"""
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
     return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Removes the current SQAlchemy Session"""
+def teardown(exception):
+    """Remove session"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port='5000')
